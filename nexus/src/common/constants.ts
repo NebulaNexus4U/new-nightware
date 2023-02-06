09050af1d1efd852be0de5ce34802d0bde5bde35
Abdul Wadudh Samdani <abdul.wadudh@wyzebulb.com>
2023-02-06 10:45:44 +0530

    feat: :bug: Added Nexus Base URL

--- nexus/src/common/constants.ts
+++ nexus/src/common/constants.ts
@@ -2,12 +2,14 @@
 export const multerUploadDest = "/nexus/src/uploads/";
 export const bodyParserLimit = "5mb";
 
+const { NEXUS_BASE_URL } = process.env;
+
 // Other Constants if Any Goies Here with there FileName
 
 export const googleOAuth = {
   clientID: "625750956269-pohhrq3cnd84o04f5c2uq3f6hqlpd5ea.apps.googleusercontent.com",
   clientSecret: "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",
-  callbackURL: "http://localhost:4000/auth/google/callback",
+  callbackURL: `${NEXUS_BASE_URL}/auth/google/callback`,
   params: { scope: ["email", "profile"] },
   state: true,
 };
@@ -15,7 +17,7 @@ export const googleOAuth = {
 export const facebookOAuth = {
   clientID: "585310313101357",
   clientSecret: "29e16df96509031a53a03f1fc51ec309",
-  callbackURL: "http://localhost:4000/auth/facebook/callback",
+  callbackURL: `${NEXUS_BASE_URL}/auth/facebook/callback`,
   params: { scope: ["email", "user_location"] },
   profileFields: ["id", "displayName", "photos", "email"],
 };
