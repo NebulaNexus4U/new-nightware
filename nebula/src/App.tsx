2e0cf40af9b6ad36bc7ce93417dd1612dfdaa365
Mohammed Abdul Wadudh Samdani <abdulwadudh5@gmail.com>
2023-02-11 20:01:45 +0530

    feat: :sparkles: Migrated to Turbo Repo

--- nebula/src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="flex justify-center p-52">
-      <span className="text-6xl">This is New Nightmare App</span>
-    </div>
-  );
-}
--- nebula/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-  readonly VITE_NODE_ENV: string;
-  readonly VITE_NEXUS_BASE_URL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
