c6cfd3bdde169b4d95597eabe0ba4f230855d043
Abdul Wadudh Samdani <abdul.wadudh@wyzebulb.com>
2023-02-14 12:01:23 +0530

    feat: :sparkles: Adjusted Viewheight

--- apps/nebula/src/App.tsx
+++ apps/nebula/src/App.tsx
@@ -7,7 +7,9 @@ export default function App() {
   return (
     <div className="h-screen">
       <Navbar color={themeColor} />
-      <div className={`relative bg-gradient-to-r from-${themeColor}-500 to-${themeColor}-400 min-h-full`}>
+      <div
+        className={`relative bg-gradient-to-r from-${themeColor}-500 to-${themeColor}-400 min-h-[89.9vh] flex items-center justify-center`}
+      >
         <RouterProvider router={newNightmareRoutes} />
       </div>
     </div>
